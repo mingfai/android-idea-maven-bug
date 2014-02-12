1ad972c26660560114a5f45d1b41af888632828a
mingfai.ma <mingfai.ma@gmail.com>
2014-02-13 03:05:55 +0800

    added a simple project with cardslib

--- idea-maven-bug/src/main/java/com/github/mingfai/HelloAndroidActivity.java
+++ idea-maven-bug/src/main/java/com/github/mingfai/HelloAndroidActivity.java
@@ -2,28 +2,32 @@ package com.github.mingfai;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
 
     /**
      * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.text);
+        //java.lang.NoClassDefFoundError: uk.co.senab.actionbarpulltorefresh.library.R$styleable
+        //text.setText(uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout.class.getName());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.github.mingfai.R.menu.main, menu);
-	return true;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.github.mingfai.R.menu.main, menu);
+        return true;
     }
 
 }
